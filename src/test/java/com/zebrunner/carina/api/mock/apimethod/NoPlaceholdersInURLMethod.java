9b12089e0aeb46f3cdaaafd48866b937405f5c74
Andrei Kamarouski <akamarouski@solvd.com>
2023-03-09 13:57:10 +0300

    initial commit

---
+++ src/test/java/com/zebrunner/carina/api/mock/apimethod/NoPlaceholdersInURLMethod.java
@@ -0,0 +1,9 @@
+package com.zebrunner.carina.api.mock.apimethod;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+
+public class NoPlaceholdersInURLMethod extends AbstractApiMethodV2 {
+    public NoPlaceholdersInURLMethod() {
+        super();
+    }
+}
